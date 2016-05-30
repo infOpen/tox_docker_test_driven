19c8622b77eea64f35b651cc8539af75df6e7f5d
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-05-30 06:44:15 +0200

    WIP - Testing

--- conftest.py
+++ conftest.py
@@ -1,66 +1,121 @@
-import pytest
+"""
+Ansible role testing base module
+"""
+
+import logging
+import logging.config
+import os
 import re
 import testinfra
+import pytest
 
 
-DOCKER_IMAGES = ['infopen/ubuntu-trusty-ssh']
+# Use testinfra to get a handy function to run commands locally
+local_command = testinfra.get_backend('local://').get_module('Command')
+
+# Manage logging
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': True,  # this fixes the problem
+
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True
+        }
+    }
+})
 
 
-@pytest.fixture(scope='module', params=DOCKER_IMAGES)
-def image_name(request):
+@pytest.yield_fixture(scope='function', autouse=True)
+def newline_before_logging(request):
     """
-    This fixture returns the image names to test
+    Add a new line before logging, more readeable
     """
 
-    return request.param
+    if request.config.getoption('capture') == 'no':
+        print  # new-line
+    yield
 
 
-@pytest.fixture(scope='module')
-def LocalCommand(TestinfraBackend):
-    """Run commands locally
-    Same as `Command` but run commands locally with subprocess even
-    when the connection backend is not "local".
-    Note: `LocalCommand` does NOT respect ``--sudo`` option
+def pytest_generate_tests(metafunc):
+    """
+    Overload pytest_generate_tests to set scope and image usage
     """
 
-    return testinfra.get_backend("local://").get_module("Command")
+    if "TestinfraBackend" in metafunc.fixturenames:
+
+        # Lookup "docker_images" marker
+        marker = getattr(metafunc.function, "docker_images", None)
+        if marker is not None:
+            images = marker.args
+        else:
+            # Default image
+            images = ["debian:jessie"]
+
+        # If the test has a destructive marker, we scope TestinfraBackend
+        # at function level (i.e. executing for each test). If not we scope
+        # at session level (i.e. all tests will share the same container)
+        if getattr(metafunc.function, "destructive", None) is not None:
+            scope = "function"
+        else:
+            scope = "session"
+
+        metafunc.parametrize(
+            "TestinfraBackend", images, indirect=True, scope=scope)
 
 
-@pytest.fixture(scope='module')
-def docker_image(image_name, LocalCommand):
-    return image_name
-
-
-@pytest.fixture(scope='module')
-def Docker(request, image_name, LocalCommand):
+@pytest.fixture
+def TestinfraBackend(request):
     """
     Boot and stop a docker image.
     """
 
     # Run a new container. Run in privileged mode, so systemd will start
-    docker_id = LocalCommand.check_output(
-        "docker run --privileged -d -P %s", image_name)
+    docker_id = local_command.check_output(
+        "docker run --privileged -d -P %s", request.param)
 
     def teardown():
-        LocalCommand("docker kill %s", docker_id)
-        LocalCommand("docker rm %s", docker_id)
-        LocalCommand("rm /tmp/%s", docker_id)
+        """
+        Actions to execute on fixture end of life
+        """
+
+        local_command.check_output("docker kill %s", docker_id)
+        local_command.check_output("docker rm %s", docker_id)
+        local_command.check_output("rm /tmp/%s", docker_id)
 
     # At the end of each test, we destroy the container
     request.addfinalizer(teardown)
 
     # Get Docker host mapping for container SSH port expose
-    host_ssh_port = LocalCommand.check_output(
+    host_ssh_port = local_command.check_output(
         "docker inspect --format"
         " '{{ (index (index .NetworkSettings.Ports \"22/tcp\") 0).HostPort }}'"
         " %s" % docker_id)
 
-    _manage_inventory_file(docker_id, host_ssh_port, LocalCommand)
+    is_verbose = (request.config.option.verbose > 0)
+    _manage_inventory_file(docker_id, host_ssh_port)
+    container = testinfra.get_backend("docker://%s" % docker_id)
+    set_authorized_keys(container, is_verbose)
+    provision_with_ansible_by_ssh(container, is_verbose)
 
-    return testinfra.get_backend("docker://%s" % docker_id)
+    return container
 
 
-def _manage_inventory_file(docker_id, host_ssh_port, LocalCommand):
+def _manage_inventory_file(docker_id, host_ssh_port):
     """
     Manage ansible inventory file
 
@@ -72,45 +127,58 @@ def _manage_inventory_file(docker_id, host_ssh_port, LocalCommand):
     :type LocalCommand: pytest.fixture
     """
 
-    ansible_version = LocalCommand.check_output('ansible --version')
-    is_ansible_v2 = re.match('^ansible\s+2.*', ansible_version)
+    ansible_version = local_command.check_output('ansible --version')
+    is_ansible_v2 = re.match(r'^ansible\s+2.*', ansible_version)
     ansible_prefix = 'ansible_ssh'
 
     if is_ansible_v2:
         ansible_prefix = 'ansible'
 
-    inventory_content = ("{1} {0}_port={2} {0}_host=127.0.0.1\n".format(
-                         ansible_prefix, docker_id, host_ssh_port))
+    inventory_content = ("[{1}]\n{1} {0}_port={2} {0}_host=127.0.0.1\n".format(
+        ansible_prefix, docker_id, host_ssh_port))
 
     with open('/tmp/{}'.format(docker_id), 'w') as tmp_inventory:
         tmp_inventory.write(inventory_content)
 
 
-def set_authorized_keys(self):
+def set_authorized_keys(container, is_verbose):
     """
     Set authorized keys content for root user into container
     """
 
-    with open('/home/alexandre/.ssh/id_rsa.pub') as public_key_file:
-        self.run('mkdir /root/.ssh')
-        self.run('echo "%s" > /root/.ssh/authorized_keys' %
-                 public_key_file.read())
+    public_key_file_path = os.environ.get('SSH_PUBLIC_KEY_PATH')
+    with open(public_key_file_path) as public_key_file:
+        public_key = public_key_file.read()
+        container.run('mkdir /root/.ssh')
+        container.run('echo "%s" > /root/.ssh/authorized_keys' % public_key)
+
+        if is_verbose:
+            logger = logging.getLogger('set_authorized_keys')
+            logger.info('Add public SSH key: %s', public_key)
 
 
-def provision_with_ansible_by_ssh(self, LocalCommand):
+def provision_with_ansible_by_ssh(container, is_verbose):
     """
     Provision the image with Ansible
     """
 
-    cmd = LocalCommand(
-            "ANSIBLE_SSH_CONTROL_PATH=./%%h-%%r "
-            "ANSIBLE_PRIVATE_KEY_FILE=/home/alexandre/.ssh/id_rsa "
-            "ANSIBLE_REMOTE_USER=root "
-            "ANSIBLE_HOST_KEY_CHECKING=False "
-            "ANSIBLE_SSH_PIPELINING=True "
-            "ansible -m setup {0} -i /tmp/{0}".format(self.name))
+    # TODO: Check to display output if -v
+    private_key_file_path = os.environ.get('SSH_PRIVATE_KEY_PATH')
+    cmd = local_command(
+        "ANSIBLE_SSH_CONTROL_PATH=./%%h-%%r "
+        "ANSIBLE_PRIVATE_KEY_FILE={1} "
+        "ANSIBLE_REMOTE_USER=root "
+        "ANSIBLE_HOST_KEY_CHECKING=False "
+        "ANSIBLE_SSH_PIPELINING=True "
+        "ANSIBLE_ROLES_PATH={2}/../:{2}:{3} "
+        "ansible-playbook -i /tmp/{0} ./testing_deployment.yml".format(
+            container.name, private_key_file_path,
+            os.path.dirname(os.getcwd()), os.getcwd()))
+
+    if is_verbose:
+        logger = logging.getLogger('provision_with_ansible_by_ssh')
+        logger.info('Ansible provision command:\n%s', cmd.command)
+        logger.info('Execute ansible provision:\n%s', cmd.stdout)
+
     assert cmd.rc == 0
     return cmd
-
-testinfra.backend.docker.DockerBackend.set_authorized_keys = set_authorized_keys
-testinfra.backend.docker.DockerBackend.provision_as = provision_with_ansible_by_ssh
--- tests/testinfra/test_docker.py
+++ tests/testinfra/test_docker.py
@@ -1,10 +1,16 @@
+"""
+Role tests
+"""
 import pytest
 
+# Not wokg :/
 # To mark all the tests as destructive:
 # pytestmark = pytest.mark.destructive
 
 # To run all the tests on given docker images:
-# pytestmark = pytest.mark.docker_images("debian:jessie", "centos:7")
+pytestmark = pytest.mark.docker_images('infopen/ubuntu-trusty-ssh')
+#pytestmark = pytest.mark.docker_images('infopen/ubuntu-trusty-ssh',
+#                                       'infopen/ubuntu-xenial-ssh')
 
 # Both
 # pytestmark = [
@@ -13,34 +19,5 @@ import pytest
 # ]
 
 
-# This test will run on default image (debian:jessie)
-@pytest.fixture(scope="module", autouse=True)
-@pytest.mark.docker_images('infopen/ubuntu-trusty-ssh', 'infopen/ubuntu-xenial-ssh')
-def provision(Docker, LocalCommand):
-    Docker.set_authorized_keys()
-    Docker.provision_as(LocalCommand)
-
 def test_foo(User):
-    assert User().name == 'alexandre'
-#def test_default(Process):
-#    assert Process._backend.name == ''
-#    assert Process.get(pid=1).comm == "tail"
-#def test_default_2(Process):
-#    assert Process._backend.name == ''
-#    assert Process.get(pid=1).comm == "tail"
-
-
-# This test will run on both debian:jessie and centos:7 images
-#@pytest.mark.docker_images("debian:jessie", "centos:7")
-#def test_multiple(Process):
-#    assert Process.get(pid=1).comm == "tail"
-
-
-# This test is marked as destructive and will run on its own container
-# It will create a /foo file and run 3 times with different params
-#@pytest.mark.destructive
-#@pytest.mark.parametrize("content", ["bar", "baz", "qux"])
-#def test_destructive(Command, File, content):
-#    assert not File("/foo").exists
-#    Command.check_output("echo %s > /foo", content)
-#    assert File("/foo").content == content + "\n"
+    assert User().name == 'root'
